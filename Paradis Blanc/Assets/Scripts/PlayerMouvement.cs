f7518f2b8655aaedbd010791a7c945c5987ccdb7
Unknown <lucafloreau@akeonet.com>
2019-01-25 11:12:50 +0100

    air + bloom effect

--- Paradis Blanc/Assets/Scripts/PlayerMouvement.cs
+++ Paradis Blanc/Assets/Scripts/PlayerMouvement.cs
@@ -9,10 +9,18 @@ public class PlayerMouvement : MonoBehaviour
     [SerializeField] private float speed;
 
 
+    [SerializeField] private float airMax; // Comme les chaussures XD
+    public float AirMax => airMax;
+    
+    [SerializeField] private float decreaseAir;
+    private float actualAir;
+    public float ActualAir => actualAir;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        actualAir = airMax;
     }
 
     // Update is called once per frame
@@ -21,6 +29,34 @@ public class PlayerMouvement : MonoBehaviour
         if (Input.GetButton("Jump") || Input.touchCount>0)
         {
             rigidbody2D.AddForce(Vector2.down*speed);
+        }
+
+        actualAir -= decreaseAir * Time.deltaTime;
+        if (actualAir <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        LivesManagement.Instance.health = 0;
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Surface"))
+        {
+            actualAir = airMax;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Surface"))
+        {
+            actualAir = airMax;
         }
     }
 }
---
+++ Paradis Blanc/Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private Slider airSlider;
+
+    [SerializeField] private GameObject postprocessGameObject;
+
+    [SerializeField] private float pourcentageBloomActivation; // pourcentage à partir dulequel le bloom s'active
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        airSlider.maxValue = GameManager.Instance.Player.AirMax;
+        airSlider.value = GameManager.Instance.Player.ActualAir;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        airSlider.value = GameManager.Instance.Player.ActualAir;
+        if (GameManager.Instance.Player.ActualAir <= GameManager.Instance.Player.AirMax * (pourcentageBloomActivation/100))
+        {
+            postprocessGameObject.SetActive(true);
+        }
+        else
+        {
+            postprocessGameObject.SetActive(false);
+        }
+    }
+}
