905bb7ca08667edfcfacd265051bb8ee7cb43eb8
Tan <doriantan7@gmail.com>
2019-01-26 16:22:17 +0100

    avancementLVL, menu,pause

---
+++ Paradis Blanc/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject MenuPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void BtnStart()
+    {
+        SceneManager.LoadScene("Scenes/MainScene");
+        Time.timeScale = 1f;
+    }
+
+    public void BtnCredits()
+    {
+        creditsPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+
+    }
+
+    public void BtnMenu()
+    {
+        creditsPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+
+
+    public void BtnExit()
+    {
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+}
---
+++ Paradis Blanc/Assets/Scripts/Pause.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject ui;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle() // marche pour le bouton "continue"
+    {
+        ui.SetActive(!ui.activeSelf); //plus simple pour basculer d'un état à l'autre
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle(); //être sûr que le temps est arrêter
+        SceneManager.LoadScene("Scenes/MainScene");
+
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+}
